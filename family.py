b802aad6b4135a26feae7a79ff49b6565ff5d398
Dollywobbles <72455239+Dollywobbles@users.noreply.github.com>
2020-10-06 12:34:26 -0400

    Add files via upload

---
+++ family.py
@@ -0,0 +1,46 @@
+print('I Love Our Family!')
+
+Tim = {
+'name': 'Tim',
+'age': 35,
+'hair': 'red',
+'eyes': 'blue',
+'favorite color': 'green'
+	}
+
+Liz = {
+'name': 'Liz',
+'age' : 35,
+'hair': 'brown',
+'eyes': 'blue',
+'favorite color': 'green'
+	}
+
+Kayley = {
+'name': 'Kayley',
+'age' : 11,
+'hair': 'brown',
+'eyes': 'blue',
+'favorite color': 'blue'
+	}
+
+Liam = {
+'name': 'Liam',
+'age' : 9,
+'hair': 'blonde',
+'eyes': 'blue',
+'favorite color': 'blue'
+	}
+
+Daisy = {
+'name': 'Daisy',
+'age' : 7,
+'hair': 'brown',
+'eyes': 'brown',
+'favorite color': 'yellow'
+	}
+
+family = [Tim, Liz, Kayley, Liam, Daisy]
+
+print(family[name])
+
